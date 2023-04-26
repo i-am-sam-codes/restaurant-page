67d9e0f10a76a84d20a96565bc407824d5d6c76d
Sam Ceja <sammmceja@gmail.com>
2023-04-26 16:43:28 -0700

    add title to home module

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import nav from "./nav.js";
+import createHome from "./pages/home.js";
 
 function component() {
   const element = document.createElement("div");
 
   document.addEventListener("DOMContentLoaded", nav);
+  document.addEventListener("DOMContentLoaded", createHome);
 
   return element;
 }
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,4 +1,10 @@
-function createHome() {
+export default function createHome() {
   const contentDiv = document.getElementById("content");
+  const homeContainer = document.createElement("div");
+
+  const title = document.createElement("h1");
+  title.textContent = "Welcome to the cat cafe!";
+
+  homeContainer.appendChild(title);
+  contentDiv.appendChild(homeContainer);
 }
-export { createHome };
